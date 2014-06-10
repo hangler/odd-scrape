4849bdb3c726101963d44044c3c1b309104038bc
hangler <fhangler@gmail.com>
2014-06-10 16:54:28 +0100

    Initial commit

---
+++ austria.py
@@ -0,0 +1,171 @@
+from scrape_helper import *
+import pprint
+import time
+
+class AustriaAppDirectory(AbstractAppDirectory):
+
+    @classmethod
+    def listing_url(self):
+        return "http://www.data.gv.at/anwendungen/page/"
+
+    @classmethod
+    def num_pages(self):
+        return 25
+
+    def detail_urls(self, page=1):
+        detail_pages = []
+        for row in self.soup.find('ul', {'class': 'entryAnwendungen'}).findAll('li'):
+            detail_url = row.find('a').get('href')
+            detail_pages.append(detail_url)
+        return detail_pages
+
+class AustriaAppDetail(AbstractAppDetail):
+
+    def app_detail_url(self):
+        return self.page_url
+
+    def g_title(self):
+        return self.soup.find('h1', {'class': 'nomargin'}).find('span').text
+   
+    """
+    This actually finds the creator's URL.
+    def g_url(self):
+        for h3 in self.soup.find('div', {'id': 'contentSidebar'}).findAll('h3'):
+            if "Kontakt" in h3.text:
+                for item in h3.next_siblings:
+                    if item.name is not None and "p" in item.name:
+                        return item.find('a').get('href')
+    """
+
+    def g_url(self):
+        return self.soup.find('a', {'class': 'buttonBlue'}).get('href')
+
+    def g_summary(self):
+        summary = ""
+        first_para = self.soup.find('div', {'class': 'contentText'}).find('p')
+        summary = first_para.text
+        for item in first_para.next_siblings:
+            if item.name is None:
+                summary = summary + "\n"
+            elif item.get('class') is None:
+                summary = summary + "\n" + item.text
+            else:
+                break
+        return summary
+    
+    def g_lang(self):
+        return "DE"
+    
+    def g_country(self):
+        return "Austria"
+    
+    def g_created_for(self):
+        pass
+    
+    def g_source_code_url(self):
+        pass
+    
+    def g_platforms(self):
+        platforms = []
+        for h3 in self.soup.find('div', {'id': 'contentSidebar'}).findAll('h3'):
+            if "Systeme" in h3.text:
+                for item in h3.next_siblings:
+                    if item.name is not None and 'p' in item.name:
+                        for link in item.findAll('a'):
+                            index = link.get('href').find("applicationsystem") + len("applicationsystem=")
+                            platform = link.get('href')[index:]
+                            if "android" in platform:
+                                platforms.append("Android")
+                            if "ios" in platform:
+                                platforms.append("iOS")
+                            if "browser" in platform:
+                                platforms.append("Web")
+                            if "windows" in platform:
+                                platforms.append("Windows Phone")
+                        break
+        return platforms
+
+    def g_technology(self):
+        pass
+    
+    def g_developers(self):
+        developers = []
+
+        developer = None
+        
+        name = None
+        email = None
+        homepage = None
+
+        for h3 in self.soup.find('div', {'id': 'contentSidebar'}).findAll('h3'):
+            if "Kontakt" in h3.text:
+                for item in h3.next_siblings:
+                    if len(item) > 1:
+                        if item.name is not None and 'p' in item.name:
+                            email_at_index = item.text.find("@")
+                            if email_at_index:
+                                email_begin_index = item.text.rfind(" ", 0, email_at_index) + 1
+                                email_end_index = item.text.find(" ", email_at_index)
+                                email = item.text[email_begin_index:email_end_index].strip()
+                            link = item.find('a')
+                            if link:
+                                homepage = link.get('href')
+                        else:
+                            if not name:
+                                name = item.strip()
+
+        if name:
+            developer = Developer(is_organization=False, name=name, country="Austria", email=email, homepage=homepage)
+
+        developers.append(developer)
+        return developers
+    
+    def g_data_sources(self):
+        data_sources = []
+
+        if self.soup.find('table'):
+            for row in self.soup.find('table').findAll('tr'):
+                link = row.find('a')
+                if link:
+                    url = link.get('href')
+                    link_text = link.text
+                    index_of_first_parenthesis = link_text.find("(")
+                    name = link_text[:index_of_first_parenthesis]
+                    source = link_text[index_of_first_parenthesis+1:-1]
+                    data_source = DataSource(name, country="Austria", url=url, organization=source)
+                    data_sources.append(data_source)
+        
+        return data_sources
+
+"""
+listing_url = AustriaAppDirectory.listing_url() + "1"
+page = requests.get(listing_url).text
+soup = BeautifulSoup(page)
+aad = AustriaAppDirectory(soup)
+
+pp = pprint.PrettyPrinter(indent = 2)
+
+for url in aad.detail_urls():
+    print url
+    app = AustriaAppDetail(url)
+    pp.pprint(app.visit())
+    print "\n"
+"""
+
+t = TabWriter("output.tab")
+t.write_headers()
+
+for page in range(1, AustriaAppDirectory.num_pages()+1):
+    listing_url = AustriaAppDirectory.listing_url() + str(page)
+    page = requests.get(listing_url).text
+    soup = BeautifulSoup(page)
+    aad = AustriaAppDirectory(soup)
+    for url in aad.detail_urls():
+        print url
+        app = AustriaAppDetail(url)
+        t.write_row(app)
+        time.sleep(2)
+
+
+#app1 = AustriaAppDetail("foo", soup)
+#print app1.visit()
---
+++ scrape_helper.py
@@ -0,0 +1,145 @@
+from abc import ABCMeta, abstractmethod
+from bs4 import BeautifulSoup
+import inspect
+import requests
+
+class Developer(object):
+
+    def __init__(self, is_organization, name, country=None, email=None, homepage=None):
+        self.is_organization = is_organization
+        self.name = name
+        self.country = country
+        self.email = email
+        self.homepage = homepage
+
+    def __unicode__(self):
+        return u"[%s, %s (%s, %s)]" % (self.name, self.country, self.email, self.homepage)
+
+class DataSource(object):
+
+    def __init__(self, name, country=None, url=None, organization=None, description=None):
+        self.name = name
+        self.country = country
+        self.url = url
+        self.organization = organization
+        self.description = description
+
+    def __unicode__(self):
+        return u"[%s (%s, %s) %s]" % (self.name, self.organization, self.country, self.url)
+
+class AbstractAppDirectory(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, soup):
+        self.soup = soup
+
+    @abstractmethod
+    def detail_urls(self):
+        pass
+
+    @abstractmethod
+    def listing_url(self):
+        pass
+
+    @abstractmethod
+    def num_pages(self):
+        pass
+
+class AbstractAppDetail(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, page_url):
+        self.page_url = page_url
+        self.page = requests.get(page_url).text
+        self.soup = BeautifulSoup(self.page)
+
+    @classmethod
+    def headers(self):
+        headers = []
+        for func in inspect.getmembers(self, predicate=inspect.ismethod):
+            if "g_" in func[0]:
+                function_name = func[0][2:] # Strip g_ prefix from function name
+                headers.append(function_name)
+        return "\t".join(headers) + "\n"
+
+    def visit(self):
+        values = {}
+        for func in inspect.getmembers(self, predicate=inspect.ismethod):
+            if "g_" in func[0]:
+                function_name = func[0][2:] # Strip g_ prefix from function name
+                values[function_name] = func[1]()
+        #   if isinstance(attr_value, types.FunctionType):
+        #        print attr_value
+        return values
+
+    @abstractmethod
+    def g_title(self):
+        pass
+
+    @abstractmethod
+    def g_url(self):
+        pass
+
+    @abstractmethod
+    def g_summary(self):
+        pass
+
+    @abstractmethod
+    def g_lang(self):
+        pass
+
+    @abstractmethod
+    def g_country(self):
+        pass
+
+    @abstractmethod
+    def g_created_for(self):
+        pass
+
+    @abstractmethod
+    def g_source_code_url(self):
+        pass
+    
+    @abstractmethod
+    def g_platforms(self):
+        pass
+
+    @abstractmethod
+    def g_technology(self):
+        pass
+
+    @abstractmethod
+    def g_developers(self):
+        pass
+
+    @abstractmethod
+    def g_data_sources(self):
+        pass
+
+class TabWriter(object):
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def write(self, text, tab=True, mode="a"):
+        f = open(self.file_name, mode)
+        f.write(text.encode('utf-8'))
+        f.close()
+
+    def write_headers(self):
+         self.write(AbstractAppDetail.headers(), tab=False, mode="w")
+
+    def write_row(self, app_detail):
+        row = ""
+        if isinstance(app_detail, AbstractAppDetail):
+            app_data = app_detail.visit()
+            for k in sorted(app_data):
+                value = app_data[k]
+                if isinstance(value, list):
+                    row = row + ", ".join(unicode(x) for x in value) + "\t"
+                elif value is None:
+                    row = row + "\t"
+                else:
+                    row = row + value.replace("\n", "") + "\t"
+        row = row + "\n"
+        self.write(row, tab=False)  
